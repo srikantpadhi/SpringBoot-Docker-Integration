9c5b298a32a8a10dca9461114f3c38d6317924b8
Padhi <Srikant.Padhi@rsa.com>
2020-01-21 13:00:39 +0530

    First Commit

---
+++ src/main/java/com/example/demo/SpringbootDockerIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerIntegrationApplication.class, args);
+	}
+
+}
